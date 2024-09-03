356a018e76985e03daa0216c35c2e69a5fd91731
Vincent Montero Fontaine <vincent.montero-fontaine@epitech.eu>
2024-09-03 09:59:50 +0200

    Environment variables

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,22 @@ const { Pool } = require('pg');
 const app = express();
 const port = 5000;
 
+// Récupérer variables d'environnement
+require('dotenv').config();
+
 // Configurer le middleware
 app.use(cors());
 app.use(bodyParser.json());
 
 // Configurer la connexion à PostgreSQL
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'survivor',
-  password: 'postgres',
-  port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
 });
+
 
 // Endpoint pour récupérer les employés
 app.get('/api/employees', async (req, res) => {
